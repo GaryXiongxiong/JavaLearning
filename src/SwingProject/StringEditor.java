b63b975e276bd8f7b9da5c08957d735ef08543b2
GaryXiongXiong <i@jiangyixiong.top>
2019-05-17 15:14:55 +0800

    Switch

--- src/SwingProject/StringEditor.java
+++ src/SwingProject/StringEditor.java
@@ -1,4 +1,27 @@
 package SwingProject;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class StringEditor {
+public class StringEditor extends JFrame {
+    public StringEditor(){
+        this.setTitle("String Editor");
+        this.setBounds(120,120,400,500);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        Container c = this.getContentPane();
+        JButton read = new JButton("Read");
+        JButton write = new JButton("Write");
+        JTextArea content = new JTextArea(50,50);
+        JScrollPane sp = new JScrollPane(content);
+        c.setLayout(new FlowLayout());
+        c.add(read);
+        c.add(write);
+        c.add(sp);
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        StringEditor se1 = new StringEditor();
+    }
 }
